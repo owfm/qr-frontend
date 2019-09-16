c46ca1b661afc9ed8f954e76cd4cb4f417aed610
Oliver Mansell <oliverwfmansell@gmail.com>
2019-09-16 13:30:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dataList, setDataList] = useState([]);
+  const [qrs, setQrs] = useState(null);
+  const handleChange = event => {
+    setDataList(event.target.value.split(",").map(item => item.trim()));
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log();
+    fetch("http://localhost:5000/qr", {
+      method: "POST",
+      body: JSON.stringify({ data: dataList }), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(response => response.json())
+      .then(data => setQrs(data));
+  };
+
+  const QRs = () => {
+    if (!qrs) return null;
+    return qrs.map(qr => {
+      return (
+        <>
+          <img
+            alt={`${qr.split("/").pop()}`}
+            src={`http://localhost:5000/${qr}`}
+          />
+          <p>{qr.split("/")[1].split(".")[0]}</p>
+        </>
+      );
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Input">
+        <form onSubmit={handleSubmit}>
+          <textarea name="datalist" onChange={handleChange} />
+          <p>Enter a list of data, comma separated.</p>
+          <button submit>Generate QR Codes</button>
+        </form>
+        <QRs />
+      </div>
     </div>
   );
 }
